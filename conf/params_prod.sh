232e57dae5d3eb566270a8083a171416dc8146a9
waqas.yousaf <waqas.yousaf@flaconi.de>
2024-07-31 14:20:34 +0200

    OPS-6023: variablised

--- conf/params_prod.sh
+++ conf/params_prod.sh
@@ -1,13 +1,13 @@
 # Stack parameter
-export ORG_ACCOUNT_ID='' # ID for Organization Management account 
+export ORG_ACCOUNT_ID="$2"  # ID for Organization Management account 
 export ORG_ROLE=OrganizationsReadOnlyAccess
-export AWS_REGION=eu-west-1
+export AWS_REGION=eu-central-1
 export EXTERNAL_ID='' #Optional 
 export JIRA_DEFAULT_ASSIGNEE='' #ID for default assignee for all Security Issues
-export JIRA_INSTANCE="" #HTTPS address for JIRA server (exclude schema "https://")
-export JIRA_PROJECT_KEY="" # JIRA Project Key
-export ISSUE_TYPE="" #JIRA Issuetype name: Example, "Bug", "Security Issue"
-export REGIONS=("eu-west-1") # List of regions deployed
+export JIRA_INSTANCE="$3"  #HTTPS address for JIRA server (exclude schema "https://")
+export JIRA_PROJECT_KEY="$4"  # JIRA Project Key
+export ISSUE_TYPE="Task" #JIRA Issuetype name: Example, "Bug", "Security Issue"
+export REGIONS=("eu-central-1") # List of regions deployed
 
 PARAMETERS=(
   "OrganizationManagementAccountId=$ORG_ACCOUNT_ID"
